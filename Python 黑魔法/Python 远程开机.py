261f163924637f46e1b6b6825711d1add91ca146
22chiragA <122338784+22chiragA@users.noreply.github.com>
2023-02-09 10:40:22 +0530

    Update Python 远程开机.py

--- Python 黑魔法/Python 远程开机.py
+++ Python 黑魔法/Python 远程开机.py
@@ -9,20 +9,3 @@ def wake_up(request, mac='DC-4A-3E-78-3E-0A'):
 
     # 把原始数据转换为16进制字节数组，
     for i in range(0, len(data), 2):
-        send_data = b''.join([send_data, struct.pack('B', int(data[i: i + 2], 16))])
-    print(send_data)
-
-    # 通过socket广播出去，为避免失败，间隔广播三次
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        sock.sendto(send_data, (BROADCAST, 7))
-        time.sleep(1)
-        sock.sendto(send_data, (BROADCAST, 7))
-        time.sleep(1)
-        sock.sendto(send_data, (BROADCAST, 7))
-        return HttpResponse()
-        print("Done")
-    except Exception as e:
-        return HttpResponse()
-        print(e)
